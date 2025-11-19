ff4612a5dfc7a4b4d2b9fa302dc8ce9ee5648f86
20Pol <alexander.15.qp@gmail.com>
2025-11-19 08:11:11 -0500

    add reserva form

--- alquiler_frontend/src/app/app.routes.ts
+++ alquiler_frontend/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './vistas/inicio/inicio.component';
 import { CatalogoComponent } from './vistas/catalogo/catalogo.component';
 import { PerfilComponent } from './vistas/perfil/perfil.component';
+import { ReservaComponent } from './vistas/reserva-form/reserva-form.component';
+
 
 
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'catalogo', component: CatalogoComponent },
-  { path: 'perfil', component: PerfilComponent }
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'reserva', component: ReservaComponent }
 ];
 
 @NgModule({
--- alquiler_frontend/src/app/vistas/reserva-form/reserva-form.component.ts
+++ alquiler_frontend/src/app/vistas/reserva-form/reserva-form.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface Vehiculo {
+  id: number;
+  marca: string;
+  modelo: string;
+}
 
 @Component({
-  selector: 'app-reserva-form',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
+  selector: 'app-reserva',
   templateUrl: './reserva-form.component.html',
-  styleUrl: './reserva-form.component.css'
+  styleUrls: ['./reserva-form.component.css']
 })
-export class ReservaFormComponent {
+export class ReservaComponent {
+  vehiculos: Vehiculo[] = [
+    { id: 1, marca: 'Toyota', modelo: 'Corolla' },
+    { id: 2, marca: 'Hyundai', modelo: 'Tucson' },
+    { id: 3, marca: 'Kia', modelo: 'Sportage' }
+  ];
 
+  reserva = {
+    vehiculoId: null as number | null,
+    fechaInicio: '',
+    fechaFin: '',
+    kilometraje: null as number | null,
+    seguro: 'ninguno',
+    comentarios: ''
+  };
+
+  cancelarReserva() {
+  // Redirigir al catálogo o página anterior
+  window.history.back(); // simple y rápido
+  // O usar el Router de Angular:
+  // this.router.navigate(['/catalogo']);
+}
+
+  reservarVehiculo() {
+    console.log('Datos de la reserva:', this.reserva);
+    // Aquí puedes llamar a un servicio para enviar al backend
+  }
 }
