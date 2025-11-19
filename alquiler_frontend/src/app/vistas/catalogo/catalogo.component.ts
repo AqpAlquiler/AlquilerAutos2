d61c5aa6a1ae5165ec656c9d2adfa740b4ba5127
20Pol <alexander.15.qp@gmail.com>
2025-11-19 06:29:05 -0500

    add catalogo

--- alquiler_frontend/src/app/vistas/catalogo/catalogo.component.ts
+++ alquiler_frontend/src/app/vistas/catalogo/catalogo.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-catalogo',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './catalogo.component.html',
   styleUrls: ['./catalogo.component.css']
 })
