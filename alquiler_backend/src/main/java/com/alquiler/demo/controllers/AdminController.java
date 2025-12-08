d96a502f06e9c4fda7604d12e8c57616d2c5b47a
deyadan7 <danidu0107@gmail.com>
2025-12-07 20:20:04 -0500

    los css

--- alquiler_backend/src/main/java/com/alquiler/demo/controllers/AdminController.java
+++ alquiler_backend/src/main/java/com/alquiler/demo/controllers/AdminController.java
@@ -1,6 +1,6 @@
 package com.alquiler.demo.controllers;
 
-import org.springframework.stereotype.Controller;
+import npm org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
